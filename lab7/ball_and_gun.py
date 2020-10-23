ca122f0b06fff8c21791bc1656c07a311d571a99
akulov artem <akulov.aa@phystech.edu>
2020-10-23 18:08:17 +0300

    raboshaya pushka

--- lab7/ball_and_gun.py
+++ lab7/ball_and_gun.py
@@ -9,22 +9,68 @@ WHITE = (255, 255, 255)
 pg.init()
 
 class Ball():
-    pass
+    def __init__(self, coord, vel, rad=10, color=None):
+        if color == None:
+            color = (randint(0, 255), randint(0, 255), randint(0, 255))
+        self.color = color
+        self.coord = coord
+        self.vel = vel
+        self.rad = rad
+
+    def draw(self, screen):
+        pg.draw.circle(screen, self.color, self.coord, self.rad)
+
+    def move(self, t_step=1.):
+        for i in range(2):
+            self.coord[i] += int(self.vel[i] * t_step)
+        self.check_walls()
+
+    def check_walls(self):
+        n = [[1, 0], [0, 1]]
+        for i in range(2):
+            if self.coord[i] < self.rad:
+                self.coord[i] = self.rad
+                self.flip_vel(n[i])
+            elif self.coord[i] > SCREEN_SIZE[i] - self.rad:
+                self.coord[i] = SCREEN_SIZE[i] - self.rad
+                self.flip_vel(n[i])
+
+    def flip_vel(self, axis, coef_perp=1., coef_par=1.):
+        vel = np.array(self.vel)
+        n = np.array(axis)
+        n = n / np.linalg.norm(n)
+        vel_perp = vel.dot(n) * n
+        vel_par = vel - vel_perp
+        ans = -vel_perp * coef_perp + vel_par * coef_par
+        print(vel, ans)
+        self.vel = ans.astype(np.int).tolist()
+
 
 class Table():
     pass
 
 class Gun():
-    def __init__(self, coord=[30, SCREEN_SIZE[1] // 2]):
+    def __init__(self, coord=[30, SCREEN_SIZE[1] // 2], min_pow=10, max_pow=30):
         self.coord = coord
         self.angle = 0
+        self.min_pow = min_pow
+        self.max_pow = max_pow
+        self.power = min_pow
+        self.active = False
 
     def draw(self, screen):
-        end_pos = [self.coord[0] + 30 * np.cos(self.angle), self.coord[1] + 30 * np.sin(self.angle)]
-        pg.draw.line(screen, WHITE, self.coord, end_pos, 5)
+        end_pos = [self.coord[0] + self.power* np.cos(self.angle), self.coord[1] + self.power * np.sin(self.angle)]
+        pg.draw.line(screen, WHITE, self.coord, end_pos, 10)
 
     def strike(self):
-        pass
+        vel = [int(self.power * np.cos(self.angle)), int(self.power * np.sin(self.angle))]
+        self.active = False
+        self.power = self.min_pow
+        return Ball(list(self.coord), vel)
+
+    def preparation(self):
+        if self.active and self.power < self.max_pow:
+            self.power += 1
 
     def set_angle(self, mouse_pos):
         self.angle = np.arctan2(mouse_pos[1] - self.coord[1],
@@ -37,15 +83,24 @@ class Manager():
     def __init__(self):
         self.gun = Gun()
         self.table = Table()
+        self.balls = []
 
     def process(self, events, screen):
         done = self.handle_events(events)
         self.draw(screen)
         self.gun.draw(screen)
+        self.move()
+        self.gun.preparation()
         return done
 
     def draw(self, screen):
         screen.fill(BLACK)
+        for ball in self.balls:
+            ball.draw(screen)
+
+    def move(self):
+        for ball in self.balls:
+            ball.move()
 
     def handle_events(self, events):
         done = False
@@ -57,7 +112,12 @@ class Manager():
                     self.gun.coord[1] -= 5
                 elif event.key == pg.K_DOWN:
                     self.gun.coord[1] += 5
-
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.gun.active = True
+            elif event.type == pg.MOUSEBUTTONUP:
+                if event.button == 1:
+                    self.balls.append(self.gun.strike())
 
         if pg.mouse.get_focused():
             mouse_pos = pg.mouse.get_pos()
