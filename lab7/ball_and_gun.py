62e1a3e452d5b64e4963a3cc39a3291313f8b8f1
akulov artem <akulov.aa@phystech.edu>
2020-10-25 03:29:51 +0300

    реализован подсчет очков, время игры

--- lab7/ball_and_gun.py
+++ lab7/ball_and_gun.py
@@ -8,15 +8,15 @@ WHITE = (255, 255, 255)
 
 pg.init()
 
+
 class Ball():
     def __init__(self, coord, vel, rad=10, color=None):
-        if color == None:
+        if color is None:
             color = (randint(0, 255), randint(0, 255), randint(0, 255))
         self.color = color
         self.coord = coord
         self.vel = vel
         self.rad = rad
-
 
     def draw(self, screen):
         pg.draw.circle(screen, self.color, self.coord, self.rad)
@@ -52,6 +52,34 @@ class Ball():
             return 0
 
 
+class Score():
+    def __init__(self, numb_used=0, numb_dest=0, numb_used_final=0, time=180, time_okr=0):
+        self.numb_used = numb_used
+        self.numb_dest = numb_dest
+        self.time = time
+        self.numb_used_final = numb_used_final
+        self.time_okr = time_okr
+
+    def score(self):
+        f1 = pg.font.Font(None, 36)
+        text1 = f1.render('очков' + ' : ' + str(self.numb_dest), 1, (255, 255, 255))
+        screen.blit(text1, (10, 10))
+        text1 = f1.render('попыток' + ' : ' + str(self.numb_used), 1, (255, 255, 255))
+        screen.blit(text1, (10, 30))
+        text1 = f1.render('времени осталось' + ' : ' + str(self.time_okr) + 'сек', 1, (255, 255, 255))
+        screen.blit(text1, (10, 570))
+
+    def final_score(self):
+        f1 = pg.font.Font(None, 36)
+        text1 = f1.render('очков' + ' : ' + str(self.numb_dest), 1, (255, 255, 255))
+        screen.blit(text1, (330, 280))
+        text1 = f1.render('попыток' + ' : ' + str(self.numb_used_final), 1, (255, 255, 255))
+        screen.blit(text1, (330, 320))
+
+    def number_time(self):
+        self.time -= 1
+        self.time_okr = int(self.time / 3) / 10
+
 
 class Gun():
     def __init__(self, coord=[30, SCREEN_SIZE[1] // 2], min_pow=10, max_pow=30):
@@ -63,7 +91,7 @@ class Gun():
         self.active = False
 
     def draw(self, screen):
-        end_pos = [self.coord[0] + self.power* np.cos(self.angle), self.coord[1] + self.power * np.sin(self.angle)]
+        end_pos = [self.coord[0] + self.power * np.cos(self.angle), self.coord[1] + self.power * np.sin(self.angle)]
         pg.draw.line(screen, WHITE, self.coord, end_pos, 10)
 
     def strike(self):
@@ -80,17 +108,17 @@ class Gun():
         self.angle = np.arctan2(mouse_pos[1] - self.coord[1],
                                 mouse_pos[0] - self.coord[0])
 
+
 class Target():
     def __init__(self, x=None, y=None, rad=None, color=None):
-        if rad == None:
+        if rad is None:
             self.rad = randint(20, 40)
-        if color == None:
+        if color is None:
             self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
-        if x == None:
+        if x is None:
             self.x = randint(100, SCREEN_SIZE[0] - 100)
-        if y == None:
+        if y is None:
             self.y = randint(100, SCREEN_SIZE[1] - 100)
-
 
     def draw(self, screen):
         pg.draw.circle(screen, self.color, (self.x, self.y), self.rad)
@@ -101,13 +129,14 @@ class Target():
         if dist < mindist:
            return 1
 
+
 class Manager():
     def __init__(self):
         self.gun = Gun()
         self.balls = []
         self.target = []
         self.new_aim()
-        self.score = 0
+        self.score = Score()
 
     def new_aim(self):
         for i in range(4):
@@ -116,27 +145,37 @@ class Manager():
     def process(self, events, screen):
         done = self.handle_events(events)
         self.draw(screen)
-        self.gun.draw(screen)
+
         self.move()
         self.gun.preparation()
         self.collide()
         self.rubbish()
+
         return done
 
-
     def draw(self, screen):
-        screen.fill(BLACK)
-        for ball in self.balls:
-            ball.draw(screen)
-        for target in self.target:
-            target.draw(screen)
+        if self.score.time > 0:
+            screen.fill(BLACK)
+            for ball in self.balls:
+                ball.draw(screen)
+            for target in self.target:
+                target.draw(screen)
+            self.score.number_time()
+            self.score.score()
+            self.gun.draw(screen)
+        else:
+            self.score.numb_used_final = self.score.numb_used
+            self.balls = []
+            self.target = []
+            screen.fill(BLACK)
+            self.score.final_score()
 
     def collide(self):
         for ball in self.balls:
             for i in range(0, len(self.target)):
                 if Target.check_collide(self.target[i], ball) == 1:
-                    self.score += 1
-                    print(self.score)
+                    self.score.numb_dest += 1
+
                     self.target.pop(i)
                     return
         if len(self.target) == 0 and len(self.balls) == 0:
@@ -147,8 +186,6 @@ class Manager():
             if Ball.check_alive(self.balls[i]) == 0:
                 self.balls.pop(i)
                 return
-
-
 
     def move(self):
         for ball in self.balls:
@@ -169,16 +206,20 @@ class Manager():
                     self.gun.active = True
             elif event.type == pg.MOUSEBUTTONUP:
                 if event.button == 1:
-                    self.balls.append(self.gun.strike())
-
+                    if self.score.time > 0:
+                        self.balls.append(self.gun.strike())
+                        self.score.numb_used += 1
+                    else:
+                        pass
         if pg.mouse.get_focused():
             mouse_pos = pg.mouse.get_pos()
             self.gun.set_angle(mouse_pos)
 
         return done
 
+
 screen = pg.display.set_mode(SCREEN_SIZE)
-pg.display.set_caption("The gun of Zepopa")
+pg.display.set_caption("The gun of Abacaba")
 clock = pg.time.Clock()
 
 mgr = Manager()
